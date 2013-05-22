217c8df87437c3714ac6808bd285a5e4dcf9e1ee
Chang Long <changlon@gmail.com>
2013-05-22 16:56:35 +0800

    add twig cache

--- app/config/prod.php
+++ app/config/prod.php
@@ -4,11 +4,11 @@ $app['debug']       = false ;
 $app['root_dir']    = __DIR__ . '/../../' ;
 
 
-$app['twig.templates']  = array(
-            
-        ) ;
+$app['twig.templates']  = array() ;
 
 $app['twig.path']  = array(
         __DIR__ . '/../../public_tpl' ,
         __DIR__ . '/../../web' ,
     );
+
+$app['twig.options.cache']  = __DIR__ . '/../cache/twig' ;
